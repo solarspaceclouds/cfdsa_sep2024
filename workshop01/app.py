1464c9da5ad7d93e341803697a3e3b85a3b85968
Wei May <chen_wei_may@htx.gov.sg>
2024-08-10 16:35:01 +0800

    Added procfile and modified app.py

--- workshop01/app.py
+++ workshop01/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 import random
-
+import os
 app = Flask(__name__)
 
 # List of quotes
@@ -19,4 +19,9 @@ def index():
     return render_template('index.html', quote=random_quote)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+
+    port = int(os.environ.get('PORT', 5000))
+
+    app.run(host='0.0.0.0', port=port)
+    
